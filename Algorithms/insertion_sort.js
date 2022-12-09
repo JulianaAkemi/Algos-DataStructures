bd6175a18c60f42b664b2dda67f12cb6777708fc
Juliana Akemi <julianaakkemi@gmail.com>
2022-12-09 08:02:58 -0300

    code styling fix

--- Algorithms/insertion_sort.js
+++ Algorithms/insertion_sort.js
@@ -1,10 +1,10 @@
 const insertionSort = (arr) => {
-  var currentVal;
+  let currentVal;
 
-  for (var i = 1; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     currentVal = arr[i];
 
-    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+    for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       arr[j + 1] = arr[j];
     }
 
